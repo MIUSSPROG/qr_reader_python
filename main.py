44df78249468818ba42ab1d919851c93e08f554e
KruzhokProg <Aa1@3$5^7>
2022-04-06 17:56:08 +0300

    cv in tkinter

--- main.py
+++ main.py
@@ -3,38 +3,98 @@ from openpyxl import Workbook, load_workbook
 import cv2
 import pandas as pd
 from pyzbar.pyzbar import decode
-import tkinter
-from tkinter import messagebox
+import tkinter as tk
+from tkinter import messagebox, CENTER
+import imutils
+from PIL import Image, ImageTk
+import PIL
 
-root = tkinter.Tk()
-root.withdraw()
+root = tk.Tk()
+root.geometry("800x600")
+# root.withdraw()
 cap = cv2.VideoCapture(0)
 
-file = 'fio.xlsx'
-xl = pd.ExcelFile(file)
-active_sheet = xl.sheet_names[0]
-wordbook = load_workbook(filename=file)
-sheet = wordbook[active_sheet]
-df = pd.read_excel(file, sheet_name=active_sheet)
-saved = df.values.tolist()
-saved = [item[0] for item in saved]
+lbimage = tk.Label()
+lbimage.pack(side="top", anchor="ne")
 
-while True:
-    success, frame = cap.read()
-    for code in decode(frame):
-        print(code.data.decode('utf-8'))
-        print(saved)
-        if str(code.data.decode('utf-8')) not in saved:
-            new_fio = code.data.decode('utf-8')
-            sheet.append([new_fio])
-            wordbook.save(filename=file)
-            df = pd.read_excel(file, sheet_name=active_sheet)
-            saved = df.values.tolist()
-            saved = [item[0] for item in saved]
-            time.sleep(1)
-        else:
-            messagebox.showinfo("Title", "Пользователь " + str(code.data.decode('utf-8')) + " уже существует!")
-    cv2.imshow('Testing-code-scan', frame)
-    cv2.waitKey(10)
+
+# lbText = tk.Label(text="Последний пользователь \nИванов Иван Иванович", font=("roboto", 30))
+# lbText.place(anchor=CENTER, relx=0.5, rely=0.5)
+
+
+# file = 'fio.xlsx'
+# xl = pd.ExcelFile(file)
+# active_sheet = xl.sheet_names[0]
+# wordbook = load_workbook(filename=file)
+# sheet = wordbook[active_sheet]
+# df = pd.read_excel(file, sheet_name=active_sheet)
+# saved = df.values.tolist()
+# saved = [item[0] for item in saved]
+
+
+def capture():
+    try:
+        file = 'fio.xlsx'
+        xl = pd.ExcelFile(file)
+        active_sheet = xl.sheet_names[0]
+        wordbook = load_workbook(filename=file)
+        sheet = wordbook[active_sheet]
+        df = pd.read_excel(file, sheet_name=active_sheet)
+        saved = df.values.tolist()
+        saved = [item[0] for item in saved]
+
+        success, frame = cap.read()
+        if success:
+            frame = imutils.resize(frame, width=320)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = PIL.ImageTk.PhotoImage(image=Image.fromarray(frame))
+            lbimage.imgtk = img
+            lbimage.configure(image=img)
+            for code in decode(frame):
+                print(code.data.decode('utf-8'))
+                # print(saved)
+                if str(code.data.decode('utf-8')) not in saved:
+                    new_fio = code.data.decode('utf-8')
+                    sheet.append([new_fio])
+                    wordbook.save(filename=file)
+                    df = pd.read_excel(file, sheet_name=active_sheet)
+                    saved = df.values.tolist()
+                    saved = [item[0] for item in saved]
+                else:
+                    lbText = tk.Label(text="Последний пользователь " + str(saved[-1]), font=("roboto", 30))
+                    lbText.place(anchor=CENTER, relx=0.5, rely=0.5)
+                    # messagebox.showinfo("Title", "Последний пользователь " + str(saved[-1]))
+            root.after(1, capture)
+    except Exception as exc:
+        print(exc)
+
+
+root.after(1000, capture)
+root.mainloop()
+
+# while True:
+#     success, frame = cap.read()
+#     frame = imutils.resize(frame, width=320)
+#     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     img = PIL.ImageTk.PhotoImage(image=Image.fromarray(frame))
+#     lbimage.imgtk = img
+#     lbimage.configure(image=img)
+# for code in decode(frame):
+#     print(code.data.decode('utf-8'))
+#     print(saved)
+#     if str(code.data.decode('utf-8')) not in saved:
+#         new_fio = code.data.decode('utf-8')
+#         sheet.append([new_fio])
+#         wordbook.save(filename=file)
+#         df = pd.read_excel(file, sheet_name=active_sheet)
+#         saved = df.values.tolist()
+#         saved = [item[0] for item in saved]
+#         time.sleep(1)
+#     else:
+#         messagebox.showinfo("Title", "Пользователь " + str(code.data.decode('utf-8')) + " уже существует!")
+# cv2.imshow('Testing-code-scan', frame)
+# cv2.waitKey(10)
+
+# root.after(1000, capture)
+# root.mainloop()
 # cap.release()
-
