10d549f4d3d70c2278f195fdc3b6bea3cbf21057
KruzhokProg <Aa1@3$5^7>
2022-04-21 13:22:11 +0300

    добавлен индикатор считывания + таймер простоя

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2
 import pandas as pd
 from pyzbar.pyzbar import decode
 import tkinter as tk
-from tkinter import messagebox, CENTER
+from tkinter import messagebox, CENTER, NW
 import imutils
 from PIL import Image, ImageTk
 import PIL
@@ -24,12 +24,20 @@ lbText = tk.Label()  # создание лэйбла для отображени
 lbText.place(anchor=CENTER, relx=0.5, rely=0.5)  # размещаем по центру
 lbTextLimitMessage = tk.Label()  # еще один лэйбл для учета лимита посещений
 lbTextLimitMessage.place(anchor=CENTER, relx=0.5, rely=0.7)  # чуть ниже предыдущего лэйбла
+good_outcome_color = '#0f0'
+bad_outcome_color = "#f00"
+canvas = tk.Canvas()
+canvas.place(anchor=NW)
+canvas.create_rectangle(10, 10, 320, 180, fill='', outline='', tags="indicator")
+# canvas.create_rectangle(10, 10, 320, 180, fill="#0f0", outline="#0f0", tags="indicator")
 
-file = 'test1.xlsx'  # название файла куда будет все сохраняться(должен быть в одном каталоге с испоняющим файлом)
-
-
+file = 'test1.xlsx'  # название файла куда будет все сохраняться(должен быть в одном каталоге с исполняющим файлом)
+timer_dist = 10*10**9
+end = time.perf_counter_ns()
 # основная функция обработки данных с камеры(работает рекурсивно)
 def capture():
+    global end
+    global canvas
     try: # для отлавливания ошибок
         xl = pd.ExcelFile(file)  # вытаскиваем метаинформацию об иксель файл
         sheets = xl.sheet_names  # вытаскиваем названия всех листов
@@ -43,9 +51,16 @@ def capture():
         # item[1] - фио
         # item[2] - класс
         # item[3] - лимит посещений
-
+        # and len(decode(frame)) != 0
         success, frame = cap.read() # читаем данные с камры
-        if success: # если все прочитано успешно
+        if success: # если камера что-то видит
+            if len(decode(frame)) == 0: # если qr кода нет
+                if abs(time.perf_counter_ns() - end) > timer_dist: # через 10 сек подчищаем экран
+                    lbText.config(text="")
+                    lbTextLimitMessage.config(text="")
+                    # canvas.delete("indicator")
+                    canvas.itemconfigure('indicator', fill='')
+
             frame = imutils.resize(frame, width=320) # выводим данные с камеры в окошко шириной 320 пкс с автоматическим подгоном высоты
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # светокоррекция(нужно когда сжимаешь изображение в строке выше)
             img = PIL.ImageTk.PhotoImage(image=Image.fromarray(frame)) # считываем что видит камера
@@ -74,28 +89,38 @@ def capture():
 
                 if user_id not in saved_visitors_id: # если его еще нет в списке
                     if user_id not in fio_list_userids:
-                        lbTextLimitMessage.config(text=f"Пользователя с таким кодом не существует")
+                        lbText.config(text=f"Пользователя с таким кодом не существует", font=("roboto", 30))
+                        lbTextLimitMessage.config(text="")
+                        canvas.itemconfigure('indicator', fill='#f00')
                     else:
                         if fio_map[user_id][3] == 0: # проверка если на листе с фио лимит 0, тогда ничего не делаем и оповещаем
                             lbTextLimitMessage.config(
                                 text=f"У пользователя {fio_map[user_id][3]} нет доступа на {today}")
+                            canvas.itemconfigure('indicator', fill='#f00')
                         else: # если лимит какой-то есть
-                            visited = fio_map[user_id] + [1] # формируем данные с дополнительм полем(посещаемость - первый раз добавляем туда 1)
+                            visited = fio_map[user_id] + [1, datetime.now().strftime('%H:%M:%S')] # формируем данные с дополнительми полями(посещаемость - первый раз добавляем туда 1 и время)
                             save_user(visited, today) # сохраняем
                             lbText.config(text="Последний пользователь\n" + str(visited[1]), font=("roboto", 30)) # выводим последнего пользователя
                             lbTextLimitMessage.config(text="") # очищаем лэйбл где ограничение лимита
                             time.sleep(2) # чтобы сразу весь лимит не использовался делаем задержку считывания с камеры qr кода в 2 секунды
-                else: # еслии есть такой уже пользователь в новом листе
+                            canvas.itemconfigure('indicator', fill='#0f0')
+                    end = time.perf_counter_ns()
+                else: # если есть такой уже пользователь в новом листе
                     if (fio_map[user_id][3] - saved_visitors_map[user_id][4]) > 0: # проверяем, хвататет ли ему лимита на очередную регистрацию на мероприятие
                         row = saved_visitors_id.index(user_id) # если хватает находим его позицию(номер строки) на листе
                         workbook[today][f'E{row + 2}'] = saved_visitors_map[user_id][4] + 1 # и в ячейку столбца "посещение" добавляем +1
                         workbook[today][f'F{row + 2}'] = datetime.now().strftime('%H:%M:%S')
                         workbook.save(filename=file) # сохраняем все
                         time.sleep(2) # очередная задержка
+                        canvas.itemconfigure('indicator', fill='#0f0')
                     else: # если не хватило лимита, то пользователь исчерпал доступный запас
                         lbTextLimitMessage.config(
                             text=f"Пользователь {saved_visitors_map[user_id][1]} исчерпал(а) лимит посещений на {today}",
                             fg='#f00')
+                        lbText.config(text="Последний пользователь\n" + str(saved_visitors[-1][1]), font=("roboto", 30))
+                        canvas.itemconfigure('indicator', fill='#f00')
+                    end = time.perf_counter_ns()
+
             root.after(1, capture) # здесь происходит самовызов функции для повторного считывания, таким образом имитируя непрерывную работу камеры
     except Exception as exc: # если какая ошибка она будет перехвачени и выведена в лог
         print(exc)
