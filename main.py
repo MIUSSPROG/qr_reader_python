df7c1dcd5f6e84067b9ae0f01a83e5dfd48e48eb
KruzhokProg <Aa1@3$5^7>
2022-06-07 12:48:25 +0300

    подгрузка картинки с firebase

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import time
 import traceback
+from io import BytesIO
 from tkinter.filedialog import askopenfile
+from tkinter.tix import Tk
 
 from openpyxl import Workbook, load_workbook
 import cv2
 import pandas as pd
 from pyzbar.pyzbar import decode
 import tkinter as tk
-from tkinter import messagebox, CENTER, NW
+from tkinter import messagebox, CENTER, NW, SW
 import imutils
 from PIL import Image, ImageTk
 import PIL
 from datetime import date, datetime
 from dateutil import parser
+import requests
+from urllib.request import urlopen
+from PIL import ImageTk
 
 root = tk.Tk()  # создание интерфейса
 root.title('Регистрация участников')
 root.geometry("800x600")  # размер интерфейса
 cap = cv2.VideoCapture(0)  # объект для захвата изображения
 
+avatarImage = tk.Label()
+avatarImage.pack(side="bottom", anchor="se")
 lbimage = tk.Label()  # создание области ввыода изображения с камеры
 lbimage.pack(side="top", anchor="ne")  # размещение справа вверху (ne - north east)
 lbText = tk.Label()  # создание лэйбла для отображения последнего пользователя
@@ -34,8 +41,8 @@ canvas.create_rectangle(10, 10, 320, 180, fill='', outline='', tags="indicator")
 
 file = 'test1.xlsx'  # название файла куда будет все сохраняться(должен быть в одном каталоге с исполняющим файлом)
 timer_dist = 10*10**9
-timer_dist_repeat = 10*60*10**9 # время задержки простоя
-timer_dist_repeat = 10 # время задержки между посещениями
+timer_dist_repeat = 10*60 # время задержки простоя
+# timer_dist_repeat = 10 # время задержки между посещениями
 end = time.perf_counter_ns()
 # основная функция обработки данных с камеры(работает рекурсивно)
 def capture():
@@ -49,12 +56,8 @@ def capture():
         df = pd.read_excel(file, sheet_name=fio_sheet) # читаем полностью все данные с первой страницы
         fio_list = df.values.tolist() # преобразуем в списко(для удобства)
         fio_list_userids = [item[0] for item in fio_list]
-        fio_map = {item[0]: [item[0], item[1], item[2], item[3]] for item in fio_list} # создаем словарь для удобного обращения к данным по userId
-        # item[0] - userId
-        # item[1] - фио
-        # item[2] - класс
-        # item[3] - лимит посещений
-        # and len(decode(frame)) != 0
+        fio_map = {item[0]: [item[0], item[1], item[2], item[3], item[4]] for item in fio_list} # создаем словарь для удобного обращения к данным по userId
+
         success, frame = cap.read() # читаем данные с камры
         if success: # если камера что-то видит
             if len(decode(frame)) == 0: # если qr кода нет
@@ -63,6 +66,7 @@ def capture():
                     lbTextLimitMessage.config(text="")
                     # canvas.delete("indicator")
                     canvas.itemconfigure('indicator', fill='')
+                    avatarImage.configure(image='')
 
             frame = imutils.resize(frame, width=320) # выводим данные с камеры в окошко шириной 320 пкс с автоматическим подгоном высоты
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # светокоррекция(нужно когда сжимаешь изображение в строке выше)
@@ -79,13 +83,14 @@ def capture():
                     workbook[today]['B1'] = 'ФИО'
                     workbook[today]['C1'] = 'Класс'
                     workbook[today]['D1'] = 'Лимит'
-                    workbook[today]['E1'] = 'Посещение'
-                    workbook[today]['F1'] = 'Время'
+                    workbook[today]['E1'] = 'Фото'
+                    workbook[today]['F1'] = 'Посещение'
+                    workbook[today]['G1'] = 'Время'
                     workbook.save(file) # если такого нет, то создаем
 
                 df = pd.read_excel(file, sheet_name=today) # читаем содержимое листа с текущей датой
                 saved_visitors = df.values.tolist() # преобразовываем в спискок
-                saved_visitors_map = {item[0]: [item[0], item[1], item[2], item[3], item[4], item[5]] for item in saved_visitors} # для удобного обращения к данным по userId делаем словарь
+                saved_visitors_map = {item[0]: [item[0], item[1], item[2], item[3], item[4], item[5], item[6]] for item in saved_visitors} # для удобного обращения к данным по userId делаем словарь
                 saved_visitors_id = [int(item[0]) for item in saved_visitors] # список с userId
 
                 user_id = int(code.data.decode('utf-8')) # расшифровываем qr код (там userId)
@@ -105,20 +110,27 @@ def capture():
                             save_user(visited, today) # сохраняем
                             lbText.config(text="Последний пользователь\n" + str(visited[1]), font=("roboto", 30)) # выводим последнего пользователя
                             lbTextLimitMessage.config(text="") # очищаем лэйбл где ограничение лимита
-                            time.sleep(0.5) # чтобы сразу весь лимит не использовался делаем задержку считывания с камеры qr кода в 2 секунды
+                            response = requests.get(fio_map[user_id][4])
+                            img_data = response.content
+                            img = Image.open(BytesIO(img_data)).resize((200, 200))
+                            img = ImageTk.PhotoImage(img)
+
+                            avatarImage.imgtk = img  # сохраняем в область для отображения
+                            avatarImage.configure(image=img)
+                            # data = urlopen(fio_map[user_id][4])
+                            # image = ImageTk.PhotoImage(data=data.read())
+                            # tk.Label(root, image=image).pack()
+                            # time.sleep(0.5) # чтобы сразу весь лимит не использовался делаем задержку считывания с камеры qr кода в 2 секунды
                             canvas.itemconfigure('indicator', fill='#0f0')
                     end = time.perf_counter_ns()
                 else: # если есть такой уже пользователь в новом листе
-                    last_saved_time = parser.parse(today + " " + saved_visitors_map[user_id][5]).timestamp()
+                    last_saved_time = parser.parse(today + " " + saved_visitors_map[user_id][6]).timestamp()
                     cur_time = datetime.now().timestamp()
                     if abs(last_saved_time - cur_time) > timer_dist_repeat:
-                        if (fio_map[user_id][3] - saved_visitors_map[user_id][4]) > 0: # проверяем, хвататет ли ему лимита на очередную регистрацию на мероприятие
-                            # print("строка в дату: " + str(last_saved_time))
-                            # print("текущее время: " + str(cur_time))
-                            # print("разница: ", abs(last_saved_time - cur_time))
+                        if (fio_map[user_id][3] - saved_visitors_map[user_id][5]) > 0: # проверяем, хвататет ли ему лимита на очередную регистрацию на мероприятие
                             row = saved_visitors_id.index(user_id) # если хватает находим его позицию(номер строки) на листе
-                            workbook[today][f'E{row + 2}'] = saved_visitors_map[user_id][4] + 1 # и в ячейку столбца "посещение" добавляем +1
-                            workbook[today][f'F{row + 2}'] = datetime.now().strftime('%H:%M:%S')
+                            workbook[today][f'F{row + 2}'] = saved_visitors_map[user_id][5] + 1 # и в ячейку столбца "посещение" добавляем +1
+                            workbook[today][f'G{row + 2}'] = datetime.now().strftime('%H:%M:%S')
                             workbook.save(filename=file) # сохраняем все
                             # time.sleep(0.5) # очередная задержка
                             lbText.config(text="Последний пользователь\n" + str(saved_visitors_map[user_id][1]), font=("roboto", 30)) # выводим последнего пользователя
